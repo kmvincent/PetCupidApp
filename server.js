c12b1a16375c0e80a8331db8abd9baa8b0ee05ab
Kathleen Vincent <kvincen4316@gmail.com>
2018-07-27 14:33:52 -0500

    Project 2 template

---
+++ server.js
@@ -0,0 +1,34 @@
+
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
+
+// Starting our Express app
+// =============================================================
+//will sync all of your databases after listening to port. 
+db.sequelize.sync().then(function(){
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
