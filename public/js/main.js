704a137e928213af231c14c5d01b00985cef6765
Amanda Malmin <amandamalmin@gmail.com>
2018-07-31 09:44:38 -0500

    starting onclick functions for index.html

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,12 @@
 // onclick functions for index.html page
 $(document).ready(function () {
-    $("#btn-large").on("click", function () {
-        console.log("clicked on da cat")
+    $("#questionnaireStart").on("click", function (event) {
+        console.log("Start Questionnaire");
+        $("#questionnaireStart").addClass("hide");
     });
+
+    // $(".btn-large").on("click", function () {
+    //     console.log("clicked on da cat")
+    // });
+
 });
