76fe5306ec1201c3b8dc76c6e069722ef7f4949c
Amanda Malmin <amandamalmin@gmail.com>
2018-07-31 11:51:30 -0500

    starting to work through saving answer logic

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,3 +1,11 @@
+// so we pull the petfinder api results based on the first how many questions
+// then from there we parse the text for keywords.
+// how do we assign points based on keywords?
+// And then rank each one. 
+
+
+// every time they answer a question, it is added to array...then api is pulled based on that array after last submit, then text parser is run on those results, and compared to my math somehow?...then can click save and it's saved to db....
+
 let questions = require("../public/js/questions");
 
 // exporting routes to be used in server.js
@@ -7,10 +15,14 @@ module.exports = function (app) {
     app.get("/", function (req, res) {
         res.render("index", { questions: questions });
     });
-
-    // route to display results page if we have one
-    app.get("/results", function (req, res) {
-        // Will need to set up another handlebars page for it as well as object to pass along
-        res.render("results", );
-    });
 };
+
+
+
+
+// this will later be added to main.js
+// let queryArray = [];
+
+// $("#submit-btn").on("click", function () {
+//     queryArray.push("somehow capture which image is active");
+// });
