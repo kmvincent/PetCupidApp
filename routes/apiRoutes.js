d4b9713f8742d30f51ca22fdd6fda5af0ef45936
LefseTime <36971616+LefseTime@users.noreply.github.com>
2018-07-31 10:02:32 -0500

    some api route stuff

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,3 +4,56 @@
 // how do we assign points based on keywords?
 
 // And then rank each one. 
+
+
+
+const express = require("express");
+
+const router = express.Router();
+
+//const burger = require("../models/burger.js");
+
+// module.exports = function (app) {
+    router.post("/pf", function (req, res) {
+        // console.log(req.body.zip);
+        // console.log(req.body.method);
+        let zip = req.body.zip;
+        var method = req.body.method;
+        var key = 'eb472865553c02a68dd15a9285d88af4';
+
+        var queryUrl = `http://api.petfinder.com/${method}?format=json&key=${key}&location=${zip}&output=full`;
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+            //doesn't console.log but can click on link in console
+        })
+        // burger.create([
+        //     "burger_name", "devoured"
+        // ], [
+        //         req.body.name, false
+        //     ], function (result) {
+        //         res.json({ id: result.insertId });
+        //     });
+    });
+// }
+
+// router.put("/api/burgers/:id", function (req, res) {
+//     var condition = "id = " + req.params.id;
+
+//     console.log("condition", condition);
+
+//     burger.update({
+//         devoured: true
+//     }, condition, function (result) {
+//         if (result.changedRows == 0) {
+//             return res.status(404).end();
+//         } else {
+//             res.status(200).end();
+//         }
+//     });
+// });
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,11 +5,12 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
-var db = require("./models");
+//var db = require("./models");
 
 // Sets up the Express app to handle data parsing
 // parse application/x-www-form-urlencoded
@@ -22,13 +23,13 @@ app.use(express.static("public"));
 
 // Routes
 // =============================================================
-require("./routes/api-routes.js")(app);
-
+let routes = require("./routes/apiRoutes.js");
+app.use(routes);
 // Starting our Express app
 // =============================================================
 //will sync all of your databases after listening to port. 
-db.sequelize.sync().then(function(){
+//db.sequelize.sync().then(function(){
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
-});
+//});
