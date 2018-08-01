48defdb61bdda5a03915bcffeecb2143a15f870d
Kathleen Vincent <kvincen4316@gmail.com>
2018-08-01 14:37:10 -0500

    results handlebars added results.js added

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,13 @@
 let questions = require("../data/questions");
+let results = require("../data/results");
 
 // exporting routes to be used in server.js
 module.exports = function (app) {
 
     // route to display the question html pages
     app.get("/", function (req, res) {
-        res.render("index", { questions: questions });
+        res.render("index", { questions: questions, results:results});
     });
 };
+
+// can we pull more than one object?
