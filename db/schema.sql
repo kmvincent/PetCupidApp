2830878d791a9bc7cd14cdb61039d819ea888749
Amanda Malmin <amandamalmin@gmail.com>
2018-08-03 13:03:07 -0500

    starting cleanup process

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,3 @@ USE pets_db;
 SELECT * FROM adopters;
 SELECT * FROM pets;
 SELECT * FROM interests;
-
-
-INSERT INTO `Adopters` (`id`,`adopter_first_name`,`adopter_last_name`,`adopter_email`,`createdAt`,`updatedAt`) VALUES (DEFAULT,'adae','awefwe','efae@c.com','2018-08-02 16:24:14','2018-08-02 16:24:14');
-INSERT INTO `Pets` (`id`,`pf_id`,`createdAt`,`updatedAt`) VALUES (DEFAULT,'42214712','2018-08-02 16:24:06','2018-08-02 16:24:06');
